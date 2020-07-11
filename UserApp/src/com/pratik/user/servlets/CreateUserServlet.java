c2a50797084a9fc8ce834191f48a1a869b902774
PratikBhadane <68144977+PratikBhadane@users.noreply.github.com>
2020-07-11 17:20:41 +0530

    Add via upload

---
+++ UserApp/src/com/pratik/user/servlets/CreateUserServlet.java
@@ -0,0 +1,82 @@
+package com.pratik.user.servlets;
+
+import java.io.IOException;
+
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+
+/**
+ * Servlet implementation class CreateUserServlet
+ */
+@WebServlet(
+		urlPatterns="/addServlet",initParams = {@WebInitParam(name="dbUrl",value="jdbc:mysql://localhost/mydb"),@WebInitParam(name="dbUser",value="root"),@WebInitParam(name="dbPassword",value="Pratik@7741")})
+public class CreateUserServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private Connection connection;
+   
+	public void init(ServletConfig config) {
+    	 try {
+    		  System.out.println("Init()");
+    		  Class.forName("com.mysql.jdbc.Driver");
+    		  connection=DriverManager.getConnection(config.getInitParameter("dbUrl"),config.getInitParameter("dbUser"),config.getInitParameter("dbPassword"));
+       	      
+    	     }catch(SQLException e) {
+    	    	 e.printStackTrace();
+    	     } catch (ClassNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+    	 }
+	
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("doPost()");
+		String firstName = request.getParameter("firstName");
+		String lastName = request.getParameter("lastName");
+		String email = request.getParameter("email");
+		String password = request.getParameter("password");
+
+		try {
+			Statement statement = connection.createStatement();
+			int result = statement.executeUpdate("insert into user values('" + firstName + "','" + lastName + "','"
+					+ email + "','" + password + "')");
+			PrintWriter out = response.getWriter();
+			if (result > 0) {
+				out.print("<H1>USER CREATED</H1");
+			} else {
+				out.print("<H1>Error Creating the User</H1>");
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	
+	public void destroy() {
+	   try {
+		   System.out.println("destroy()");
+		   connection.close();
+	} catch (SQLException e) {
+		
+		e.printStackTrace();
+	}	
+	}
+	
+
+}
